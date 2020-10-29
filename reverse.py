625ea6a46713e20f9e20d104641b541fa7be215a
= <=>
2020-10-29 16:42:44 +0530

    Created two python files

---
+++ reverse.py
@@ -0,0 +1,6 @@
+import sys
+
+a = sys.argv[1]
+
+#print(a.reverse())
+print(a[::-1])
---
+++ reverse_list.py
@@ -0,0 +1,6 @@
+x = [5,7,8,9,0]
+
+print(" list before reversing: {}".format(x))
+
+print(" list after reversing: {}".format(x[::-1]))
+#print(" list after reversing: {}".format(x.reverse()))
